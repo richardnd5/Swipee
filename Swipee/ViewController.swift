723f0d4016e1b70e258f624b4c3bf3cd121d6a7a
N. Richard <nathan.d.richard@gmail.com>
2019-03-31 18:15:12 -0700

    Initial Commit

---
+++ Swipee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swipee
+//
+//  Created by N Richard on 3/31/19.
+//  Copyright © 2019 N Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SwipeeTests/SwipeeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwipeeTests.swift
+//  SwipeeTests
+//
+//  Created by N Richard on 3/31/19.
+//  Copyright © 2019 N Richard. All rights reserved.
+//
+
+import XCTest
+@testable import Swipee
+
+class SwipeeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
